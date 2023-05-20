2d2538a97719b67d9ecfd4d8d6c55b1e560dd5e8
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-20 19:51:17 +0300

    update js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItem = ({preview, original, description}) => {
+    return `  
+     <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+
+const makeGalleryItemMarup = galleryItems
+    .map(createGalleryItem)
+    .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryItemMarup);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+ });
+
+
+    
