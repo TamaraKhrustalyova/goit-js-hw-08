1014cfcbfd58c050f355d34c733a8771dc7e99f9
TamaraKhrustalyova <122490057+TamaraKhrustalyova@users.noreply.github.com>
2023-05-21 14:29:47 +0000

    Deploying to gh-pages from @ TamaraKhrustalyova/goit-js-hw-08@1183ee9619aae202ef824bebc3b3931a56ec24d4 🚀

--- 03-feedback.d512a869.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),o=document.querySelector("input"),t=document.querySelector("textarea"),c=document.querySelector("button");console.log(e),console.log(o),console.log(t),console.log(c),e.addEventListener("input");
-//# sourceMappingURL=03-feedback.d512a869.js.map
--- 03-feedback.ec2c751c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),o=document.querySelector("input"),t=document.querySelector("textarea"),n=document.querySelector("button");console.log(e),console.log(o),console.log(t),console.log(n),e.addEventListener("input")}();
-//# sourceMappingURL=03-feedback.ec2c751c.js.map
