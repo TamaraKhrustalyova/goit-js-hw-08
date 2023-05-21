26c6aa986c5f2c3487fc6a452546ea1d48f43850
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-21 23:47:07 +0300

    update_css

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,63 @@
-// const refs = {
-//     formEl: document.querySelector('.feedback-form'),
-//     emailInp: document.querySelector('input'),
-//     messageInp: document.querySelector('textarea'),
-//     buttonEL: document.querySelector('button'),
-// };
+import throttle from "lodash.throttle";
 
-const formEl = document.querySelector('.feedback-form');
-const emailInp = document.querySelector('input');
-const messageInp = document.querySelector('textarea');
-const buttonEL = document.querySelector('button');
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onSubmit);
 
-console.log(formEl);
-console.log(emailInp);
-console.log(messageInp);
-console.log(buttonEL);
+const STORAGE_KEY = "feedback-form-state";
 
-formEl.addEventListener('input', );
+function onInput(e){
+    e.preventDefault();
+    const localData = getLocalStorageData();
+    localData[e.target.name] = e.target.value;
+    saveFormData(localData);
+}
+
+function getLocalStorageData(){
+    let localData = localStorage.getItem(STORAGE_KEY);
+    if(!localData) {
+        localData = {
+            email: "", 
+            message: "",
+        };
+        saveFormData(localData);
+        return localData;
+    }
+    return JSON.parse(localData);
+}
+
+function saveFormData(formData){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function initFormData(){
+    const savedData = getLocalStorageData();
+    const emailRef = document.querySelector('input');
+    emailRef.value = savedData.email;
+    const messageRef = document.querySelector('textarea');
+    messageRef.value = savedData.message;
+}
+
+initFormData();
+
+function onSubmit(){
+    if(validateFormData()){
+        console.log(getLocalStorageData());
+        formRef.reset();
+        localStorage.removeItem(STORAGE_KEY);
+    }
+    return;
+}
+
+function validateFormData(){
+    const formData = getLocalStorageData();
+    if(formData.email.trim() === ""){
+        alert("Будь ласка, заповніть поле email!")
+        return;
+    }
+    if(formData.message.trim() ===""){
+        alert("Будь даска, введіть повідомлення");
+        return;
+    }
+    return true;
+}
