472d2eb85843cad26c1cc9c49387e302a964ada4
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-24 13:20:36 +0300

    update js task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function onPlay(data) {
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
+
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,95 +1,5 @@
 import throttle from "lodash.throttle";
-// const throttle = require("lodash.throttle");
-
-// const formRef = document.querySelector('.feedback-form');
-// formRef.addEventListener('input', throttle(onInput, 500));
-// formRef.addEventListener('submit', onSubmit);
-
 const STORAGE_KEY = "feedback-form-state";
-
-// function getLocalStorageData(){
-//     let localData = localStorage.getItem(STORAGE_KEY);
-//     if(!localData) {
-//         localData = {
-//             email: "", 
-//             message: "",
-//         };
-//         saveFormData(localData);
-//         return localData;
-//     }
-//     return JSON.parse(localData);
-// }
-// function onInput(e){
-//     const localData = getLocalStorageData();
-//     localData[e.target.name] = e.target.value;
-//     saveFormData(localData);
-// }
-
-// function saveFormData(formData){
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function initFormData(){
-//     const savedData = getLocalStorageData();
-//     const emailRef = document.querySelector('input');
-//     emailRef.value = savedData.email;
-//     const messageRef = document.querySelector('textarea');
-//     messageRef.value = savedData.message;
-// }
-
-// initFormData();
-
-// function onSubmit(){
-        // e.preventDefault();
-//     if(validateFormData()){
-//         console.log(getLocalStorageData());
-//         e.currentTarget.reset();
-//         localStorage.removeItem(STORAGE_KEY);
-//     }
-//     return;
-// }
-
-// function validateFormData(){
-//     const formData = getLocalStorageData();
-//     if(formData.email.trim() === ""){
-//         alert("Будь ласка, заповніть поле email!")
-//         return;
-//     }
-//     if(formData.message.trim() ===""){
-//         alert("Будь даска, введіть повідомлення");
-//         return;
-//     }
-//     return true;
-// }
-
-// // Simple form
-
-// const formRef = document.querySelector('.feedback-form');
-// const textareaRef = document.querySelector('textarea');
-
-// formRef.addEventListener('submit', onSubmt);
-// textareaRef.addEventListener('input', throttle(onInput, 500));
-
-// reloadPage();
-
-// function onInput(e){
-//     const message = e.target.value;
-//     localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// function onSubmt(e){
-//     e.preventDefault();
-//     e.target.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function reloadPage(){
-//     const storageData = localStorage.getItem(STORAGE_KEY);
-
-//     if(storageData){
-//         textareaRef.value = storageData;
-//     }
-// }
 
 const refs = {
     form: document.querySelector('.feedback-form'),
@@ -97,24 +7,20 @@ const refs = {
     message: document.querySelector('textarea'),
 }
 
-
 refs.form.addEventListener('submit', onFormSubmt);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
 const formData = {};
 
-getDataFromStorage()
+getDataFromStorage();
 
 function onInput(e){
-    const savedData = localStorage.getItem(STORAGE_KEY);
-    console.log(savedData);
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmt(e){
-    e.preventDefault();
-
+        e.preventDefault();
         e.currentTarget.reset();
         console.log(formData);
         localStorage.removeItem(STORAGE_KEY);
